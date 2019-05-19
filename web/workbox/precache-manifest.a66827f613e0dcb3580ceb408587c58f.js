b3aed64b96527a33221fc711d8ec72aa09cdefe5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-19 22:20:57 +0000

    Deploy unindented/twitch-player to github.com/unindented/twitch-player.git:gh-pages

---
+++ web/workbox/precache-manifest.a66827f613e0dcb3580ceb408587c58f.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5635f42c17815fcd6cf09a3dc7546229",
+    "url": "/twitch-player/web/assets/categories.svg"
+  },
+  {
+    "revision": "cc173c81ac866c3dfa55c97addd350b5",
+    "url": "/twitch-player/web/assets/channels.svg"
+  },
+  {
+    "revision": "53207cd7fa38c0ede934973c9e5f2d02",
+    "url": "/twitch-player/web/assets/home.svg"
+  },
+  {
+    "revision": "f5d1a777598aa963245884a824024089",
+    "url": "/twitch-player/web/assets/search.svg"
+  },
+  {
+    "revision": "db8175d591dcb449d20037cf8baf55c4",
+    "url": "/twitch-player/web/assets/settings.svg"
+  },
+  {
+    "revision": "ff3ec76ba4f981bf2713",
+    "url": "/twitch-player/web/categories.js"
+  },
+  {
+    "revision": "a3f15270d92cf6481ce1",
+    "url": "/twitch-player/web/category.js"
+  },
+  {
+    "revision": "83ca9b4738b5d92fc6c8",
+    "url": "/twitch-player/web/category~channels~home.js"
+  },
+  {
+    "revision": "70f1ceb46fa50f58274b",
+    "url": "/twitch-player/web/channel.js"
+  },
+  {
+    "revision": "b0bd31d48df19eb582e6",
+    "url": "/twitch-player/web/channels.js"
+  },
+  {
+    "revision": "d41f7c9e6855550ec064e0ff6cc76aff",
+    "url": "/twitch-player/web/favicon.ico"
+  },
+  {
+    "revision": "37f45c89b06cd7681eb5",
+    "url": "/twitch-player/web/home.js"
+  },
+  {
+    "revision": "ef2baf551c94f09b3602891e7f87d8c9",
+    "url": "/twitch-player/web/index.html"
+  },
+  {
+    "revision": "b9b7c9285be1381b370e",
+    "url": "/twitch-player/web/index.js"
+  },
+  {
+    "revision": "1ca9a808edf82b13cf1b",
+    "url": "/twitch-player/web/locale-ar-translation-json.js"
+  },
+  {
+    "revision": "cf0b598ff036c2160d02",
+    "url": "/twitch-player/web/locale-de-translation-json.js"
+  },
+  {
+    "revision": "5b105ae887d0290bb8d9",
+    "url": "/twitch-player/web/locale-en-translation-json.js"
+  },
+  {
+    "revision": "cbb21936957464cbe4c0",
+    "url": "/twitch-player/web/locale-es-translation-json.js"
+  },
+  {
+    "revision": "feb58573ee01c8323a26",
+    "url": "/twitch-player/web/locale-fr-translation-json.js"
+  },
+  {
+    "revision": "485916067bee6bba6bd2",
+    "url": "/twitch-player/web/locale-he-translation-json.js"
+  },
+  {
+    "revision": "672c593c152f37dd16a0",
+    "url": "/twitch-player/web/locale-it-translation-json.js"
+  },
+  {
+    "revision": "05ba0a3c7d7d840fb2ab5e123d8e53f6",
+    "url": "/twitch-player/web/manifest.webmanifest"
+  },
+  {
+    "revision": "3c077db3269a3e8ad2fa",
+    "url": "/twitch-player/web/render.js"
+  },
+  {
+    "revision": "cdb7a9aa9eb7373c1d4b",
+    "url": "/twitch-player/web/search.js"
+  },
+  {
+    "revision": "f61db4bd578a91d06189",
+    "url": "/twitch-player/web/settings.js"
+  },
+  {
+    "revision": "fd36bd8392895ee99e95",
+    "url": "/twitch-player/web/vendors~categories~category~channels~home.js"
+  },
+  {
+    "revision": "d79dce54351a37d3d219",
+    "url": "/twitch-player/web/vendors~polyfill-core.js"
+  },
+  {
+    "revision": "b54cdf29a1f63cb976d7",
+    "url": "/twitch-player/web/vendors~polyfill-resizeobserver.js"
+  },
+  {
+    "revision": "e9a239b3636a10dc7c7c",
+    "url": "/twitch-player/web/vendors~render.js"
+  }
+]);
